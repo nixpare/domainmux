b3d919469ae4caed023622676b8985ba9c972e52
Alessio Pareto (Desktop-Pare) <alessio.pareto@nixpare.com>
2024-04-18 11:27:20 +0200

    Added parameter support

--- handler.go
+++ handler.go
@@ -32,23 +32,77 @@ func (h *handler) call(ctx *Context, w http.ResponseWriter, r *http.Request) {
 }
 
 func parseQuery(query string) (path []string, selectF selectFunc, setArgsF setArgsFunc) {
-	path = strings.Split(query, ".")
+	if strings.HasPrefix(query, "...") {
+		path = strings.Split(strings.TrimLeft(query, "."), ".")
+		path[0] = "..." + path[0]
+	} else {
+		path = strings.Split(query, ".")
+	}
 	slices.Reverse(path)
 	
 	switch {
 	case query == "*":
-		path = nil
-
 		selectF = func(ctx *Context) bool {
 			return len(ctx.path) != 0
 		}
+
+		path = nil
 		
 	case path[len(path)-1] == "*":
-		path = path[:len(path)-1]
-
 		selectF = func(ctx *Context) bool {
 			return len(ctx.path) != 0
 		}
+
+		path = path[:len(path)-1]
+	
+	case strings.HasPrefix(path[len(path)-1], ":"):
+		key := strings.TrimLeft(path[len(path)-1], ":")
+
+		if strings.HasSuffix(path[len(path)-1], "?") {
+			key = strings.TrimRight(key, "?")
+
+			selectF = func(ctx *Context) bool {
+				return len(ctx.path) == 1 || len(ctx.path) == 0
+			}
+		} else {
+			selectF = func(ctx *Context) bool {
+				return len(ctx.path) == 1
+			}
+		}
+		
+		setArgsF = func(ctx *Context) {
+			if len(ctx.path) == 1 {
+				ctx.args[key] = ctx.path[0]
+			} else {
+				ctx.args[key] = ""
+			}
+		}
+
+		path = path[:len(path)-1]
+
+	case strings.HasPrefix(path[len(path)-1], "..."):
+		key := strings.TrimLeft(path[len(path)-1], ".")
+
+		if strings.HasSuffix(path[len(path)-1], "?") {
+			key = strings.TrimRight(key, "?")
+
+			selectF = func(ctx *Context) bool {
+				return true
+			}
+		} else {
+			selectF = func(ctx *Context) bool {
+				return len(ctx.path) != 0
+			}
+		}
+
+		setArgsF = func(ctx *Context) {
+			pathCopy := make([]string, len(ctx.path))
+			copy(pathCopy, ctx.path)
+			slices.Reverse(pathCopy)
+			ctx.args[key] = strings.Join(pathCopy, ".")
+		}
+
+		path = path[:len(path)-1]
 		
 	default:
 		selectF = func(ctx *Context) bool {
